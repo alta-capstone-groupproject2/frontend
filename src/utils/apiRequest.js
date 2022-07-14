7c09bcf6c87d35a9d308a808dc559b23eff42dda
rizqiirfand <rizqiirfan23@gmail.com>
2022-07-14 21:27:42 +0700

    update login

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://virtserver.swaggerhub.com/Alfin7007/lamiApp/1.0/";
+axios.defaults.baseURL = "3.88.194.152:80/";
 
 export const apiRequest = async (url, method, body, content_type) => {
   var config = {
