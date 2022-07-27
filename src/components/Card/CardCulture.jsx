76ebc28ebe2bbe105bb8588b8fd99ba9f2c33b55
rizqiirfand <48740816+rizqiirfand@users.noreply.github.com>
2022-07-27 13:26:01 +0700

    Update CardCulture.jsx

--- src/components/Card/CardCulture.jsx
+++ src/components/Card/CardCulture.jsx
@@ -2,7 +2,7 @@
 
 const CardCulture = ({ name, city, image, onClickCulture }) => {
 	return (
-		<div id='card-event' className='bg-red-700 text-white flex flex-col rounded-md overflow-hidden hover:shadow-lg cursor-pointer' onClick={onClickEvent}>
+		<div id='card-culture' className='bg-red-700 text-white flex flex-col rounded-md overflow-hidden hover:shadow-lg cursor-pointer' onClick={onClickCulture}>
 			<div className="h-40 flex items-center bg-white">
 				<img src={image} alt={name} className='h-full w-full object-contain' />
 			</div>
