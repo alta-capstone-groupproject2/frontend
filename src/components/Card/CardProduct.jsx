60787450806953c0f13b9d1e44bfb3264254eb09
rizqiirfand <48740816+rizqiirfand@users.noreply.github.com>
2022-07-27 13:31:54 +0700

    Update CardProduct.jsx

--- src/components/Card/CardProduct.jsx
+++ src/components/Card/CardProduct.jsx
@@ -1,5 +1,5 @@
 /** @format */
-
+import CurrencyFormat from 'react-currency-format';
 const CardProduct = ({ name, price, image, onClickProduct }) => {
 	return (
 		<div id='card-product' className='bg-red-700 text-white flex flex-col rounded-md overflow-hidden hover:shadow-lg cursor-pointer' onClick={onClickProduct}>
@@ -8,7 +8,7 @@ const CardProduct = ({ name, price, image, onClickProduct }) => {
 			</div>
 			<div className='px-8 py-3 font-bold'>
 				<h1 id='event-name'>{name}</h1>
-				<p id='event-city-location'>{city}</p>
+				<CurrencyFormat value={price} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} prefix={'Rp. '} />
 			</div>
 		</div>
 	);
